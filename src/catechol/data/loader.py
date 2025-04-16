34e7bfd39824f0fbedfaf9209a62686872610423
Toby Boyne <tobyboyne@gmail.com>
2025-04-16 18:05:14 +0100

    Run pre-commit

--- src/catechol/data/loader.py
+++ src/catechol/data/loader.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from catechol.data.data_labels import TARGET_LABELS
 
@@ -17,17 +17,19 @@ def load_single_solvent_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     return experiments[input_cols], experiments[TARGET_LABELS]
 
 
-def train_test_split(df: pd.DataFrame, train_percentage: float, seed: int | None = None):
+def train_test_split(
+    df: pd.DataFrame, train_percentage: float, seed: int | None = None
+):
     """Split a dataframe into a train/test pair.
-    
+
     This is a naive way of splitting data. For evaluation with cross validation,
-    and for methods where entire trajectories are observed (ODE), we should think 
+    and for methods where entire trajectories are observed (ODE), we should think
     about splitting based on experiment number."""
 
     num_train = int(len(df) * train_percentage)
     rng = np.random.default_rng(seed)
     train_idcs = rng.choice(len(df), size=num_train, replace=False)
-    
+
     train_idcs_mask = np.zeros((len(df),), dtype=bool)
     train_idcs_mask[train_idcs] = True
-    return df[train_idcs_mask], df[~train_idcs_mask] 
+    return df[train_idcs_mask], df[~train_idcs_mask]
