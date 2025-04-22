7c4d3f1fef60a74fe8ff8c5e3e5d8a16c106a1a0
Toby Boyne <48383196+TobyBoyne@users.noreply.github.com>
2025-04-22 11:24:37 +0100

    Leave one out split (#7)
    * Add `generate_leave_one_out_splits`
    
    * Flip test/train

--- scripts/simple_regression.py
+++ scripts/simple_regression.py
@@ -1,16 +1,28 @@
 from catechol.data.data_labels import INPUT_LABELS_SINGLE_SOLVENT
-from catechol.data.loader import load_single_solvent_data, train_test_split
+from catechol.data.loader import (
+    generate_leave_one_out_splits,
+    load_single_solvent_data,
+    train_test_split,
+)
 from catechol.models import GPModel
 
 model = GPModel(featurization="acs_pca_descriptors")
-train_X, train_Y = load_single_solvent_data()
+X, Y = load_single_solvent_data()
 # remove unnecessary columns
-train_X = train_X[INPUT_LABELS_SINGLE_SOLVENT]
+X = X[INPUT_LABELS_SINGLE_SOLVENT]
 
-train_X, test_X = train_test_split(train_X, train_percentage=0.8, seed=1)
-train_Y, test_Y = train_test_split(train_Y, train_percentage=0.8, seed=1)
+# you can split the data into train/test sets
+# however, this is naive as it ignores the time-series nature of the data
+train_X, test_X = train_test_split(X, train_percentage=0.8, seed=1)
+train_Y, test_Y = train_test_split(Y, train_percentage=0.8, seed=1)
 
 model.train(train_X, train_Y)
 
+predictions = model.predict(test_X)
+print(predictions)
+
+# you can also use leave-one-out splits of the data
+(train_X, train_Y), (test_X, test_Y) = next(generate_leave_one_out_splits(X, Y))
+model.train(train_X, train_Y)
 predictions = model.predict(test_X)
 print(predictions)
--- src/catechol/data/loader.py
+++ src/catechol/data/loader.py
@@ -1,5 +1,6 @@
-from pathlib import Path
 import warnings
+from pathlib import Path
+from typing import Any, Generator
 
 import numpy as np
 import pandas as pd
@@ -24,12 +25,14 @@ def train_test_split(
     """Split a dataframe into a train/test pair.
 
     This is a naive way of splitting data. For evaluation with cross validation,
-    and for methods where entire trajectories are observed (ODE), we should 
+    and for methods where entire trajectories are observed (ODE), we should
     split based on experiment number."""
 
-    warnings.warn("Since this data is a time series, this train/test split "
-                  "leads to easy interpolation, and is not a good reflection of "
-                  "model performance. Please use a different split method.")
+    warnings.warn(
+        "Since this data is a time series, this train/test split "
+        "leads to easy interpolation, and is not a good reflection of "
+        "model performance. Please use a different split method."
+    )
 
     num_train = int(len(df) * train_percentage)
     rng = np.random.default_rng(seed)
@@ -38,3 +41,25 @@ def train_test_split(
     train_idcs_mask = np.zeros((len(df),), dtype=bool)
     train_idcs_mask[train_idcs] = True
     return df[train_idcs_mask], df[~train_idcs_mask]
+
+
+def generate_leave_one_out_splits(
+    X: pd.DataFrame, Y: pd.DataFrame
+) -> Generator[
+    tuple[tuple[pd.DataFrame, pd.DataFrame], tuple[pd.DataFrame, pd.DataFrame]],
+    Any,
+    None,
+]:
+    """Generate all leave-one-out splits across the solvents.
+
+    For each split, one of the solvents will be removed from the training set to
+    make a test set.
+    """
+
+    all_solvents = X["SOLVENT NAME"].unique()
+    for solvent_name in sorted(all_solvents):
+        train_idcs_mask = X["SOLVENT NAME"] != solvent_name
+        yield (
+            (X[train_idcs_mask], Y[train_idcs_mask]),
+            (X[~train_idcs_mask], Y[~train_idcs_mask]),
+        )
